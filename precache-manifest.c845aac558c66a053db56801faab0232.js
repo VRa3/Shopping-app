4184aeff7a72280a52f1b7f662c20e1df044fd09
Vra3 <vrafal333@gmail.com>
2019-01-05 17:17:11 +0100

    Updates

---
+++ precache-manifest.c845aac558c66a053db56801faab0232.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f77e9877d4042e15a39c7cf1d47ad4a",
+    "url": "/Shopping-app/static/media/shoes-o.5f77e987.jpg"
+  },
+  {
+    "revision": "dc5a299fa1a4b69c8672520ed53df7e7",
+    "url": "/Shopping-app/static/media/flower-o.dc5a299f.jpg"
+  },
+  {
+    "revision": "430dc142e6136a7120570d29c660fc30",
+    "url": "/Shopping-app/static/media/bag-o.430dc142.jpg"
+  },
+  {
+    "revision": "15c9b73f50d71035b48a",
+    "url": "/Shopping-app/static/js/runtime~main.15c9b73f.js"
+  },
+  {
+    "revision": "0acc30289bc52c0c962b",
+    "url": "/Shopping-app/static/js/main.0acc3028.chunk.js"
+  },
+  {
+    "revision": "3dd03f4eced71baf6d3b",
+    "url": "/Shopping-app/static/js/1.3dd03f4e.chunk.js"
+  },
+  {
+    "revision": "0acc30289bc52c0c962b",
+    "url": "/Shopping-app/static/css/main.573a2efb.chunk.css"
+  },
+  {
+    "revision": "ae679e7cc68dfd0404f867ef59db1a1e",
+    "url": "/Shopping-app/index.html"
+  }
+];
