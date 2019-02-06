a3aa6e77586b68eb49eeb6694022552d90a948a6
Rafał Janus <rafal.janus.94@gmail.com>
2019-02-06 18:50:41 +0100

    Small CSS fixes + description to category-item added

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -15,11 +15,16 @@ class Product extends Component {
         return (
             <div className="[ col-md-4 ]">
                 <div className="category-item">
-                        <span className="category-item__name">
-                            {this.props.name}
-                        </span>
-                    <img src={this.props.img} alt=""
-                         className="img-fluid category-item__img"/>
+                    <span className="category-item__name">
+                        {this.props.name}
+                    </span>
+
+                    <img src={this.props.img} alt="" className="img-fluid category-item__img"/>
+
+                    <div className="category-item__description">
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius error est harum iste magnam quis repellat sunt tempore vitae voluptate!
+                    </div>
+
                     <div className="category-item__price-tag">
                         {this.props.price}
                     </div>
