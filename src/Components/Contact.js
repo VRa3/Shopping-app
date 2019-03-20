255b8c5afc22b50239d44dd13048dc4062ff9791
Rafał Janus <rafal.janus.94@gmail.com>
2019-03-20 13:59:56 +0100

    Contact form added

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,13 +1,56 @@
 import React, { Component } from 'react';
+import facebook from '../static/icons/facebook.png';
 
 class contact extends Component {
     render() {
         return (
-            <main>
-                <div className="container">
-                    <p className="heading heading--large">
-                        This is only test page for About/Contact route.
-                    </p>
+            <main className='contact-page-wrapper'>
+                <section className='contact-page-section'>
+                    <h1>Need more informations?</h1>
+                    <h2>We are ready to answer.</h2>
+                    <form className='contact-form'>
+                        <div className='form-group'>
+                            <label htmlFor='contactName'>Just name</label>
+                            <input type='email' className='form-control'
+                                   id='contactName'
+                                   placeholder='Johnny'/>
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='exampleFormControlInput1'>Email address</label>
+                            <input type='email' className='form-control'
+                                   id='exampleFormControlInput1'
+                                   placeholder='name@example.com'/>
+                        </div>
+                        <div className='form-group'>
+                            <label htmlFor='exampleFormControlTextarea1'>
+                                What's on your mind?
+                            </label>
+                            <textarea className='form-control'
+                                      id='exampleFormControlTextarea1'
+                                      rows='3'
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <button className='contact-form__send-btn'>
+                            <span>
+                                Send?
+                            </span>
+                            </button>
+                        </div>
+
+                        <div className='form-group contact-form__or-separator'>
+                            <span>
+                                or
+                            </span>
+                        </div>
+                        <p className='contact-form__log-in'>
+                            <img src={facebook} alt=""/>
+                            Live chat via facebook
+                        </p>
+                    </form>
+                </section>
+                <div className='main-banner main-banner--01'>
                 </div>
             </main>
         );
