4c7f199dfaea15a1d3268596eb954761bd9d1bf7
Rafał Janus <rafal.janus.94@gmail.com>
2019-03-25 12:55:20 +0100

    Add product animation fixed

--- src/Components/AddButton.js
+++ src/Components/AddButton.js
@@ -2,43 +2,35 @@ import React from "react";
 import Checkmark from "./contact-form/checkmark";
 
 class AddButton extends React.Component {
-    state = {
-        isAdded: false,
-    };
-
-    animateElement = (element, cb) => {
-        element.classList.add('addedAnimation');
-        setTimeout(() => {
-            element.classList.add('added');
-            cb();
-        }, 1400);
-    };
-
-    checkElement = () => {
+    render() {
         const {productsInBag} = this.props;
         const {name} = this.props.reference.props;
         if (productsInBag.find(product => product.props.name === name)) {
-            this.setState({isAdded: true});
+            return (
+                <button
+                    onClick={(event) => {
+                            this.props.addProduct(this.props.reference);
+                        }
+                    }
+                    className='category-item__button added'
+                >
+                    <Checkmark weight='bold' />
+                </button>
+            )
+        } else {
+            return (
+                <button
+                    onClick={(event) => {
+                            this.props.addProduct(this.props.reference);
+                        }
+                    }
+                    className='category-item__button'
+                >
+                    +
+                </button>
+            )
         }
-    };
 
-    componentDidMount() {
-        this.checkElement();
-    }
-
-    render() {
-        return (
-            <button
-                onClick={(event) => {
-                    this.props.addProduct(this.props.reference);
-                    this.animateElement(event.target, this.checkElement);
-                }
-                }
-                className={this.state.isAdded ? 'category-item__button added' : 'category-item__button'}
-            >
-                {this.state.isAdded ? <Checkmark weight='bold' /> : '+'}
-            </button>
-        )
     }
 }
 
